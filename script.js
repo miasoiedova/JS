bae1cea4ff4e7426ec063184a54605423d56da08
Anastasiia Miasoiedova <a.myasoedova@icloud.com>
2023-03-04 18:36:17 +0200

    Arrays have added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*Ділення, множення і т.п
+
 let firstNumber;
 let secondNumber;
 
@@ -25,4 +27,50 @@ if (firstNumber == '' || secondNumber == '') {
             } else {
         alert (`The result of division ${divisionResult}`)
             }
+        }*/
+
+const arr = [
+    23048,
+    37362,
+    4972,
+    85792,
+    94614,
+    37244,
+    3725,
+    3947652,
+    38472,
+    123
+];
+
+/*Сума елементів масиву*/ 
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number'){
+    sum = sum + arr[i]
+    }
+};
+
+/*Найбільше та найменше значення*/
+let max = arr[0];
+let min = arr[0]; 
+
+for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+        if (max < arr[i]) {
+            max = arr [i];
         }
+        if (min > arr[i]) {
+            min = arr [i];
+        }
+    }
+}
+
+/*Ялиночка*/
+let row = '#';
+for (let i = 1; i <= 5; i++) {
+    console.log (row);
+    row += '#';
+}
+
+
+console.log (sum, max, min);
